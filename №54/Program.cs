a27b633f33afced21d786700441f0e5894836b40
Ivan_Kalinyuk <ivan_kalinyuk01@mail.ru>
2022-09-08 03:40:36 +0300

    Решили №54

---
+++ №54/Program.cs
@@ -0,0 +1,47 @@
+﻿void PrintArray(int[,] matr )
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+        }
+    Console.WriteLine( );
+    }
+}
+
+void FillArray(int[,] matr )
+{
+    for (int i=0; i<matr.GetLength(0); i++)
+    {
+        for (int j=0; j<matr.GetLength(1); j++)
+        {
+            matr [i,j] = new Random().Next(1,10); //[1,10)
+        }
+    }
+}
+Console.WriteLine("Введите число строк Вашего массива");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число столбцов Вашего массива");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Вот Ваш массив, заполненный произвольными числами:");
+int[,] matrix = new int [m,n];
+FillArray(matrix);
+PrintArray(matrix);
+for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int k=0; k<matrix.GetLength(1); k++)
+        {
+          for (int j=0; j<matrix.GetLength(1)-1; j++)
+          {
+            if ( matrix [i, j+1]>matrix [i, j])
+            {
+                int help = matrix [i, j];
+                matrix [i, j] = matrix [i, j+1];
+                matrix [i, j+1] = help;
+            }
+          }
+        }  
+    }
+Console.WriteLine("А вот этот же массив, заполненный в порядке убывания построчно:");
+PrintArray(matrix);
